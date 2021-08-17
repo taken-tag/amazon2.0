c957bb4f6f51b01b3105f0c91c70765d61bd94f7
yash <flecsingh@gmail.com>
2021-08-17 23:52:50 +0530

    commit 1

---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Header from "../components/Header"
+import Banner from "../components/Banner"
+import ProductFeed from "../components/ProductFeed"
+
+
+
+export default function Home({products}) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+       <Header/>
+
+       <main className="mx-w-screen-2xl mx-auto">
+         {/* banner */}
+         <Banner/>
+
+         {/* productfeed */}
+
+         <ProductFeed products={products}/>
+       </main>
+
+    </div>
+  );
+}
+
+export async function getServerSideProps(context){
+  const products = await fetch("https://fakestoreapi.com/products").then(
+      (res) => res.json()
+  );
+
+  return{
+    props: {
+     products
+    },
+  };
+}
+
+// Get https://fakestoreapi.com/products

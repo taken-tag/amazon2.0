82a4c7752ee93975f86e2eb5c95ce4d3d4184cbf
yash <flecsingh@gmail.com>
2021-08-26 01:34:35 +0530

    commit 5

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -10,9 +10,9 @@ const app = !admin.apps.length ? admin.initializeApp({
 
 // Establish connection to stripe
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
 
-const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+const endpointSecret = `${process.env.STRIPE_SIGNING_SECRET}`;
 
 const fulfillOrder = async (session) => {
     // console.log('Fulfilling order', session)
